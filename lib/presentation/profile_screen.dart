30365632c57a4c37b08f195511c0b233475fb514
Matan Tal <matantal2@gmail.com>
2022-05-18 19:07:06 +0300

    add speech sccreen widget

--- lib/presentation/profile_screen.dart
+++ lib/presentation/profile_screen.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
 import '../main.dart';
+import 'package:speech_to_text/speech_to_text.dart';
+import 'package:highlight_text/highlight_text.dart';
+import 'package:avatar_glow/avatar_glow.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
---
+++ lib/presentation/speech_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class SpeechScreen extends StatefulWidget {
+  const SpeechScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SpeechScreen> createState() => _SpeechScreenState();
+}
+
+class _SpeechScreenState extends State<SpeechScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    );
+  }
+}
