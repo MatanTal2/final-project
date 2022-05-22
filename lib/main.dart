57fbac7a8d1f497bdaeadf4598c42e2ae15f322a
Matan Tal <matantal2@gmail.com>
2022-05-22 23:02:14 +0300

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:final_project/presentation/auth_page.dart';
-import 'package:final_project/presentation/authentication.dart';
 import 'package:final_project/presentation/home_page.dart';
 import 'package:final_project/presentation/profile_screen.dart';
 import 'package:final_project/presentation/utils.dart';
-import 'package:final_project/presentation/utils.dart';
-import 'package:final_project/presentation/utils.dart';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
@@ -39,36 +37,6 @@ class MyAp extends StatelessWidget {
 
 
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // Navigates to a new page
-  void _pushPage(BuildContext context, Widget page) {
-    Navigator.of(context) /*!*/ .push(
-      MaterialPageRoute<void>(builder: (_) => page),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    bool isConnect = true;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Name of the app'),
-      ),
-      body: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            print("size box");
-            return const ProfileScreen();
-          }
-          return const Authentication();
-        },
-      ),
-    );
-  }
-}
 
 
 class MainPage extends StatelessWidget {
