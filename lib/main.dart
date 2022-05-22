2c29751f5d2f046974960e6126a39d7213274342
Matan Tal <matantal2@gmail.com>
2022-05-22 21:02:43 +0300

    refactor main

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,42 @@
+import 'package:final_project/presentation/auth_page.dart';
 import 'package:final_project/presentation/authentication.dart';
-import 'package:final_project/presentation/login.dart';
-import 'package:final_project/presentation/register.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+import 'package:final_project/presentation/home_page.dart';
 import 'package:final_project/presentation/profile_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  //await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
-  runApp(const MaterialApp(home: MyApp()));
+
+  runApp(const MyAp());
 }
+final navigatorKey = GlobalKey<NavigatorState>();
+
+class MyAp extends StatelessWidget {
+  const MyAp({Key? key}) : super(key: key);
+  static const String title = 'Speech to text';
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    navigatorKey: navigatorKey,
+    debugShowCheckedModeBanner: false,
+    title: title,
+    theme: ThemeData.dark().copyWith(
+      colorScheme: ColorScheme.fromSwatch(
+        primarySwatch: Colors.teal,
+      ),
+    ),
+    home: const MainPage(),
+  );
+}
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
 
   // Navigates to a new page
   void _pushPage(BuildContext context, Widget page) {
@@ -35,7 +54,7 @@ class MyApp extends StatelessWidget {
       ),
       body: StreamBuilder<User?>(
         stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot){
+        builder: (context, snapshot) {
           if (snapshot.hasData) {
             print("size box");
             return const ProfileScreen();
@@ -45,4 +64,32 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+}
+
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            //optional
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasError) {
+              return const Center(
+                child: Text("Something went wrong!"),
+              );
+            } else if (snapshot.hasData) {
+              return const HomePage();
+            } else {
+              return const AuthPage();
+            }
+          },
+        ),
+      );
 }
