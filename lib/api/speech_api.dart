f9f0a39a7d0966f8d7fd98a4ecf7b0e15dca5599
Matan Tal <matantal2@gmail.com>
2022-05-24 10:22:00 +0300

    add hebrew recognazier

--- lib/api/speech_api.dart
+++ lib/api/speech_api.dart
@@ -8,18 +8,31 @@ class SpeechApi {
     required Function(String text) onResult,
     required ValueChanged<bool> onListening,
   }) async {
-    if (_speech.isListening){
+    if (_speech.isListening) {
       _speech.stop();
       return true;
     }
     final isAvailable = await _speech.initialize(
-      onStatus: (status)=> onListening(_speech.isListening),
+      finalTimeout: Duration(minutes: 1),
+      //TODO test for timeout, maybe give the user choose.
+      onStatus: (status) => onListening(_speech.isListening),
       onError: (e) => print('Error $e'),
     );
 
+    var locales = await speechLanguagePick();
+    // TODO
+    // Some UI or other code to select a locale from the list
+    // resulting in an index, selectedLocale
+
     if (isAvailable) {
-      _speech.listen(onResult: (value) => onResult(value.recognizedWords));
+      _speech.listen(
+          localeId: 'iw_IL',
+          onResult: (value) => onResult(value.recognizedWords));
     }
     return isAvailable;
+  }
+
+  static Future<List<LocaleName>> speechLanguagePick() async {
+    return await _speech.locales();
   }
 }
